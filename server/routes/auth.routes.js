d7ee3ba3e9f3219b0a1f1785bbdaf4004884d025
Erdit Bushati <erdit21@gmail.com>
2026-04-27 19:17:36 +0300

    Wire auth routes into API - add /signup and /me aliases

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -4,7 +4,9 @@ const authController = require('../controllers/auth.controller');
 const validate = require('../middleware/validate');
 
 router.post('/register', validate.register, authController.register);
-router.post('/login', validate.login, authController.login);
-router.get('/profile', authController.verifyToken, authController.profile);
+router.post('/signup',  validate.register, authController.register); // alias
+router.post('/login',   validate.login,    authController.login);
+router.get('/me',       authController.verifyToken, authController.profile); // alias
+router.get('/profile',  authController.verifyToken, authController.profile);
 
 module.exports = router;
