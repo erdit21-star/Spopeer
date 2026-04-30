51f7ae1a1cc973439bf5c5bf020ba800a125d97e
Erdit Bushati <erdit21@gmail.com>
2026-04-30 18:36:37 +0300

    Allow self-signed SMTP cert

--- server/services/email.js
+++ server/services/email.js
@@ -40,7 +40,11 @@ function getTransporter() {
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS
-    }
+    },
+    tls: {
+      rejectUnauthorized: false
+    },
+    connectionTimeout: 10000
   });
 }
 
