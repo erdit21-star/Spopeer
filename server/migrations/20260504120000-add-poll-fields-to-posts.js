8ff1f63b0aae678f7bfdb9f9598c604ffc6e5493
Erdit Bushati <erdit21@gmail.com>
2026-05-10 13:07:56 +0300

    Fix migration idempotency for post poll fields

--- server/migrations/20260504120000-add-poll-fields-to-posts.js
+++ server/migrations/20260504120000-add-poll-fields-to-posts.js
@@ -2,28 +2,43 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('posts', 'type', {
-      type: Sequelize.STRING(30),
-      allowNull: false,
-      defaultValue: 'post'
-    });
+    const table = await queryInterface.describeTable('posts');
 
-    await queryInterface.addColumn('posts', 'pollOptions', {
-      type: Sequelize.JSONB,
-      allowNull: true,
-      defaultValue: null
-    });
+    if (!table.type) {
+      await queryInterface.addColumn('posts', 'type', {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+        defaultValue: 'post'
+      });
+    }
 
-    await queryInterface.addColumn('posts', 'pollVotes', {
-      type: Sequelize.JSONB,
-      allowNull: true,
-      defaultValue: null
-    });
+    if (!table.pollOptions) {
+      await queryInterface.addColumn('posts', 'pollOptions', {
+        type: Sequelize.JSONB,
+        allowNull: true,
+        defaultValue: null
+      });
+    }
+
+    if (!table.pollVotes) {
+      await queryInterface.addColumn('posts', 'pollVotes', {
+        type: Sequelize.JSONB,
+        allowNull: true,
+        defaultValue: null
+      });
+    }
   },
 
   async down(queryInterface) {
-    await queryInterface.removeColumn('posts', 'pollVotes');
-    await queryInterface.removeColumn('posts', 'pollOptions');
-    await queryInterface.removeColumn('posts', 'type');
+    const table = await queryInterface.describeTable('posts');
+    if (table.pollVotes) {
+      await queryInterface.removeColumn('posts', 'pollVotes');
+    }
+    if (table.pollOptions) {
+      await queryInterface.removeColumn('posts', 'pollOptions');
+    }
+    if (table.type) {
+      await queryInterface.removeColumn('posts', 'type');
+    }
   }
 };
